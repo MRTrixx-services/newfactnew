ff142bc28af06ce3e1d6df4097f448c05a297380
smiley dinesh <smileydinesheee@gmail.com>
2026-05-17 16:14:04 +0530

    fixed production settings

--- newfact/settings.py
+++ newfact/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-qcmeq759(gp$5=+-0$sjhp7^^ilii*1zeo9q%f!&s^sav4s+fq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "187.77.145.144",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -115,4 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'home/static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
