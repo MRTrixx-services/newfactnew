c7da105485b9f11afa7e8bceb2bf445d660b085a
smiley dinesh <smileydinesheee@gmail.com>
2026-05-19 12:46:21 +0530

    production ready setup

--- newfact/settings.py
+++ newfact/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qcmeq759(gp$5=+-0$sjhp7^^ilii*1zeo9q%f!&s^sav4s+fq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == "True"
 
 
 ALLOWED_HOSTS = [
